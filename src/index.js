ebd617c4c000d1bc074e5957970a3eaffcb249ac
aong <yxwzaxns@gmail.com>
2022-03-25 22:51:37 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
-const { Task } = require('copha')
-
-class EmptyTask extends Task {
-    constructor() {
-        super()
-    }
-
+class EmptyTask {
     async init(){
-
+        this.log.info('some init things')
     }
-
-    async runTest() {
-    }
-
-    async run(){
-        this.log.info(`start run task:`)
-        await this.driver.open('https://copha.net')
-        this.log.info(`open web title is : ${await this.driver.getTitle()}`)
-        this.log.info(`task end.`)
-    }
-
     async runBefore(){
         this.log.info('Called before the task runs')
+    }
+    async run(){
+        this.log.info(`start run task:`)
     }
 }
 
